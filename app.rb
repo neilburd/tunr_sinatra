cf5e50f4f7b3cf55a430a52216fb0ccf01ffb70b
Adrian Maseda <amaseda@gmail.com>
2016-10-05 08:48:57 -0400

    classwork so far

--- app.rb
+++ app.rb
@@ -5,3 +5,22 @@ require "active_record"
 require_relative "db/connection"
 require_relative "models/instructor"
 require_relative "models/student"
+
+get "/instructors" do
+  @instructors = Instructor.all
+  erb :"instructors/index"
+end
+
+get "/instructors/new" do
+  erb :"instructors/new"
+end
+
+post "/instructors" do
+  @instructor = Instructor.create(params[:instructor])
+  redirect "/instructors/#{@instructor.id}"
+end
+
+get "/instructors/:id" do
+  @instructor = Instructor.find(params[:id])
+  erb :"instructors/show"
+end
