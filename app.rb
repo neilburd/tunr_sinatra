b029481d9d0138e3c7c17479e6f28a8a5eadcfcf
Adrian Maseda <amaseda@gmail.com>
2016-10-05 10:06:22 -0400

    finish in-class code

--- app.rb
+++ app.rb
@@ -15,6 +15,17 @@ get "/instructors/new" do
   erb :"instructors/new"
 end
 
+get "/instructors/:id/edit" do
+  @instructor = Instructor.find(params[:id])
+  erb :"instructors/edit"
+end
+
+put "/instructors/:id" do
+  @instructor = Instructor.find(params[:id])
+  @instructor.update(params[:instructor])
+  redirect "/instructors/#{@instructor.id}"
+end
+
 post "/instructors" do
   @instructor = Instructor.create(params[:instructor])
   redirect "/instructors/#{@instructor.id}"
@@ -23,4 +34,10 @@ end
 get "/instructors/:id" do
   @instructor = Instructor.find(params[:id])
   erb :"instructors/show"
+end
+
+delete "/instructors/:id" do
+  @instructor = Instructor.find(params[:id])
+  @instructor.destroy
+  redirect "/instructors"
 end
--- db/connection.rb
+++ db/connection.rb
@@ -2,3 +2,9 @@ ActiveRecord::Base.establish_connection(
   :adapter => "postgresql",
   :database => "wdi_app"
 )
+
+if defined? Sinatra
+	after do
+	  ActiveRecord::Base.connection.close
+	end
+end
